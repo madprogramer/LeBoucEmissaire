bfa174b0c95aa396099a57fa3766af69f219ef13
MrSkyGodz <39703795+MrSkyGodz@users.noreply.github.com>
2020-03-16 23:12:15 +0300

    last

--- Test/HUD.gd
+++ Test/HUD.gd
@@ -2,29 +2,44 @@ extends CanvasLayer
 var time_start
 var time_now
 var time
-
+var ispause = 1
 signal start_game
+var score_all = 0
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
 
-func _on_pause():
-	$in_game_menu.hide();
+func menuon():
+	score_all += time*10
+	ispause = 0
+	$menuoff.show()
+	$in_game_menu.show()
+
+func menuoff():
+	ispause = 1
+	$menuoff.hide()
+	$in_game_menu.hide()
 	
-
-
 func _ready():
 	time_start = OS.get_unix_time()
 	set_process(true)
 	
-	var menu_button = find_node("menu_button")
-	menu_button = menu_button.connect("pressed",self ,"_on_pause")
+	var menuon = find_node("menuon")
+	menuon = menuon.connect("pressed",self ,"menuon")
+	
+	var menuoff = find_node("menuoff")
+	menuoff = menuoff.connect("pressed",self ,"menuoff")
+	
+	$in_game_menu.hide();
 	
 # warning-ignore:unused_argument
 func _process(delta):
 	time_now = OS.get_unix_time()
 	time = time_now - time_start
-	update_score(time*10)
+	if(ispause):
+		update_score(score_all + time*10)
+	else:
+		
+		time_start = time_now
 	
-#_on_pause()
 	
