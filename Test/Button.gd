af4d63ba132e4874ec60297396b472016b5947ab
MrSkyGodz <39703795+MrSkyGodz@users.noreply.github.com>
2020-03-16 22:47:32 +0300

    fork
    fork

--- Test/Button.gd
+++ Test/Button.gd
@@ -3,23 +3,9 @@ var time_start
 var time_now
 var time
 
-onready var menu_button = get_node(menu_button)
-
-func update_score(score):
-	$ScoreLabel.text = str(score)
-
-
-
 func _ready():
 	time_start = OS.get_unix_time()
 	set_process(true)
 	
-# warning-ignore:unused_argument
-func _process(delta):
-	time_now = OS.get_unix_time()
-	time = time_now - time_start
-	update_score(time*10)
-	$in_game_menu.hide();
-	
-	
+
 	
--- Test/HUD.gd
+++ Test/HUD.gd
@@ -1,12 +1,30 @@
 extends CanvasLayer
+var time_start
+var time_now
+var time
 
 signal start_game
 
+func update_score(score):
+	$ScoreLabel.text = str(score)
 
-func _ready():
-	pass # Replace with function body.
-
-
-#func _process(delta):
+func _on_pause():
+	$in_game_menu.hide();
 	
 
+
+func _ready():
+	time_start = OS.get_unix_time()
+	set_process(true)
+	
+	var menu_button = find_node("menu_button")
+	menu_button = menu_button.connect("pressed",self ,"_on_pause")
+	
+# warning-ignore:unused_argument
+func _process(delta):
+	time_now = OS.get_unix_time()
+	time = time_now - time_start
+	update_score(time*10)
+	
+#_on_pause()
+	
