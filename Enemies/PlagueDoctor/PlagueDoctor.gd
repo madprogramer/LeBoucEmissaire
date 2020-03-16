5d2da98e70b1475549dd4c74fbaf1555b2723cc2
EnoughSensei <useandshed@gmail.com>
2020-03-16 23:03:02 +0300

    Hayal mi gercek mi?

--- Enemies/PlagueDoctor/PlagueDoctor.gd
+++ Enemies/PlagueDoctor/PlagueDoctor.gd
@@ -20,6 +20,14 @@ func change_direction(new_direction: String) -> void:
 	
 	current_direction = new_direction
 
+const MULTX := 300.0
+const MULTY := 100.0
+
+var last_delta: float = 0.016
+
+func _process(delta):
+	last_delta = delta
+
 func move_closer() -> void:
 	if variables.has("player") == false:
 		return
@@ -28,7 +36,8 @@ func move_closer() -> void:
 	
 	var aim_vec = player.global_position - global_position
 	aim_vec = aim_vec.normalized()
-	aim_vec.y = 0.4
+	aim_vec.x = aim_vec.x * last_delta * MULTX
+	aim_vec.y = 0.4 * last_delta * MULTY
 	
 	move_and_collide(aim_vec * variables["speed"])
 	
@@ -48,5 +57,5 @@ func _ready():
 	
 	._ready()
 	
-	variables["speed"] = 0.25
+	variables["speed"] = 1.0
 	get_node("EnemyStateMachine").update_variable("move_closer_range", 50.0)
