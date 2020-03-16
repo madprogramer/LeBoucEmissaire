ca4eb0eb2cb7ad4298d0da63f829d4faa6e6911e
MrSkyGodz <39703795+MrSkyGodz@users.noreply.github.com>
2020-03-16 22:33:22 +0300

    new
    commit

---
+++ Test/HUD.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+#func _process(delta):
+	
+
---
+++ Test/pause_controller.gd
@@ -0,0 +1,9 @@
+extends Container
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
