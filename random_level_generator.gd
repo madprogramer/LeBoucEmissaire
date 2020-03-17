1e2c037802f8e31b52525441cbc8f1cef7a46a3f
madprogramer <themadprogramer@gmail.com>
2020-03-18 01:47:58 +0300

    Added multi-enemy spawning

--- random_level_generator.gd
+++ random_level_generator.gd
@@ -3,6 +3,7 @@ extends Node
 class_name RandomLevelGenerator
 
 var player: Player = null
+var roofs: int = 0
 
 func generate_new_roof(position: Vector2) -> Roof:
 	var roof = preload("res://Roof/Roof.tscn").instance()
@@ -18,7 +19,7 @@ var last_generated = -1000.0
 func generate_random_doctor(position: Vector2) -> PlagueDoctor:
 	var doctor = preload("res://Enemies/PlagueDoctor/PlagueDoctor.tscn").instance()
 	doctor.position = position
-	doctor.position.x += randf() * 200.0
+	doctor.position.x += randf() * 270.0
 	doctor.position.y -= 27.0
 	doctor.set_player(player)
 	return doctor
@@ -28,8 +29,27 @@ func _process(delta) -> void:
 		return
 	
 	while last_generated < player.position.x + 1000.0:
-		var roof = generate_random_roof(Vector2(last_generated + randf() * 100.0, 0.0))
-		last_generated = roof.position.x + 2 * roof.get_size().x
-		add_child(roof)
-		var pdoctor = generate_random_doctor(roof.position)
-		add_child(pdoctor)
+		roof_gen_routine()
+
+func roof_gen_routine():
+	var roof = generate_random_roof(Vector2(last_generated + randf() * 100.0, 0.0))
+	last_generated = roof.position.x + 2 * roof.get_size().x
+	add_child(roof)
+	roofs+=1
+	#Increase probability of more doctors appearing as you go further
+	
+	#p1 probability for multiple spawns
+	#p2, probability for 3 spawns CONDITIONED on p1
+	#Count roofs generated!
+	var p1 = 100 - pow( 2.71828, -0.03125 * roofs )/0.007
+	var p2 = 100 - pow( 2.71828, -0.015625 * roofs )/0.005
+	
+	var pdoctor = generate_random_doctor(roof.position)
+	add_child(pdoctor)
+	if ( randi()%100 <= p1) :
+		var pdoctor2 = generate_random_doctor(roof.position)
+		add_child(pdoctor2)
+		if ( randi()%100 <= p2) :
+			var pdoctor3 = generate_random_doctor(roof.position)
+			add_child(pdoctor3)
+	
